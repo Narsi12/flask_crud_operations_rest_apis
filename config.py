9cde4873f40900d310a942b58713aa13e0ba3bbb
Narsi12 <87262117+Narsi12@users.noreply.github.com>
2023-07-24 17:22:12 +0530

    Update config.py
    db url is updated

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/crud_flask'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://<username>:<password>@localhost/<db_name>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
