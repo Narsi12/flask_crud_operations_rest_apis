1df971c32ff6f649d2caa833d7bf9c2b313c54ef
narsimhac <narsimhac.in@mouritech.com>
2023-07-24 17:06:58 +0530

    all crud operations are added

--- app.py
+++ app.py
@@ -26,6 +26,61 @@ def create_user():
 
     return jsonify({'message': 'User created successfully'})
 
+@app.route('/get_all_users',methods=['GET'])
+def get_all_users():
+    data = Employee.query.all()
+    result = []
+    for emp in data:
+        result.append({
+            "employee_id":emp.employee_id,
+            "name":emp.name,
+            "address":emp.address,
+            "phone":emp.phone
+        })
+    print(result)
+    return jsonify({'message':'fetch data'})
+
+@app.route('/get_all_users/<int:id>',methods=['GET'])
+def get_single_user(id):
+    user =Employee.query.filter_by(employee_id= id).first()
+    if not user :
+        return jsonify({'message':'User not found'})
+    
+    return jsonify({
+            "employee_id":user.employee_id,
+            "name":user.name,
+            "address":user.address,
+            "phone":user.phone
+        })
+
+@app.route('/update_record/<int:id>', methods=['PUT'])
+def update_user(id):
+    user =Employee.query.filter_by(employee_id= id).first()
+    if not user:
+        return jsonify({'message':'User not found'})
+    data = request.get_json()
+    name = data['name']
+    address = data['address']
+    phone = data['phone']
+
+    user.name = name
+    user.address = address
+    user.phone = phone
+
+    # Commit the changes to the database
+    db.session.commit()
+    return jsonify({'message':'User record updated successfully ! '})
+
+@app.route('/delete_record/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user =Employee.query.filter_by(employee_id= id).first()
+    if not user:
+        return jsonify({'message':'User not found'})
+    
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User record deleted successfully!'})
 
 
 if __name__=='__main__':
